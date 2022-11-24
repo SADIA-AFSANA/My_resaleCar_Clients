c2cd11b881c16195b4b3143f1619c7264a7ffb10
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-11-24 18:05:39 +0600

    firebase set

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm()
+    const handleLogin = data => {
+        console.log(data)
+    }
+
     return (
-        <div className='h-[800px] flex justify-center items-center'>
-            <h1>This is login page</h1>
+        <div className='h-[800px] flex justify-center items-center bg-orange-100'>
+            <div className='w-96 p-7'>
+                <h2 className='text-4xl '>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+
+                        </label>
+                        <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <label className="label">
+                            <span className="label-text">Forget Password?</span>
+                        </label>
+                    </div>
+                    <input className='btn btn-success' value="Login" type="submit" />
+                </form>
+                <p>New to Account <Link className='btn btn-primary' to="signin">SIGN IN</Link></p>
+                <div className='divider'>OR</div>
+                <button className='btn btn-secondary w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
